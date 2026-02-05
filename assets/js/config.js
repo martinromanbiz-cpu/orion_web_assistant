bf25d276bcee09f6ed52bb97ef1b0596fe8edf1f
martinromanbiz-cpu <martinroman.biz@gmail.com>
2026-02-05 15:32:32 +0100

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,8 @@
 // assets/js/config.js
 window.ORION_CONFIG = {
+  // n8n formulář webhook (PRODUCTION)
+  CONTACT_WEBHOOK_URL: "https://martinroman36.app.n8n.cloud/webhook/52215ffe-42b0-43b9-ba97-037e46475ab3",
+
   // n8n chat webhook
   N8N_WEBHOOK_URL: "https://martinroman36.app.n8n.cloud/webhook/71df30a1-7b93-46bc-817d-61a7da4ef1a6/chat",
 
